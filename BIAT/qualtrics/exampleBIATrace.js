58ea1e2c1966afda932ca5ceeb737d4704b9acf4
LJAndresen <100218341+LJAndresen@users.noreply.github.com>
2022-02-27 21:12:33 +0700

    Update exampleBIATrace.js

--- BIAT/qualtrics/exampleBIATrace.js
+++ BIAT/qualtrics/exampleBIATrace.js
@@ -5,9 +5,9 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/ljandresen/minno-tasks@0.*/BIAT/q
 	return iatExtension({
 		practiceCategory1 : 
 			{
-				name : 'Mammals', //Will appear in the data.
+				name : 'Mamsfsdfsdfmals', //Will appear in the data.
 				title : {
-					media : {word : 'Mammals'}, //Name of the category presented in the task.
+					media : {word : 'Mamsdfsdfsdfmals'}, //Name of the category presented in the task.
 					css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
 					height : 4, //Height (because we need to know where to put the next item in the title)
 					startStimulus : { 
