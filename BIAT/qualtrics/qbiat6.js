c1ede4702e553d1905035f41806cfb940364d9a5
LJAndresen <100218341+LJAndresen@users.noreply.github.com>
2022-02-27 20:50:14 +0700

    Update qbiat6.js

--- BIAT/qualtrics/qbiat6.js
+++ BIAT/qualtrics/qbiat6.js
@@ -44,9 +44,9 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor,Scorer, _) {
 			}, 
 			practiceCategory1 : 
 			{
-				name : '🤙', //Will appear in the data.
+				name : 'Mammals', //Will appear in the data.
 				title : {
-					media : {word : 'Mammals'}, //Name of the category presented in the task.
+					media : {word : '😬'}, //Name of the category presented in the task.
 					css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
 					height : 4, //Height (because we need to know where to put the next item in the title)
 					startStimulus : { 
